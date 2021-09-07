a84e803641a54d63727b789f72c8c97c5fe0b94b
ryanmadoni <radoni@stevens.edu>
2021-09-07 16:00:03 -0400

    added testing isPerfect

--- Binary Tree/BinaryTrees.py
+++ Binary Tree/BinaryTrees.py
@@ -681,6 +681,15 @@ class TestBinaryTreeMethods(unittest.TestCase):
         self.assertEqual(self.binaryTree3.isBalanced(), False) # Check if binaryTree3's call to the isBalanced() function yields the correct output.
         self.assertEqual(self.binaryTree4.isBalanced(), True) # Check if binaryTree4's call to the isBalanced() function yields the correct output.
 
+    def testIsPerfect(self):
+        """
+        A function to test the functionality of the class BinaryTree's isPerfect()'s function.
+        """
+        self.assertEqual(self.binaryTree1.isPerfect(), False) # Check if binaryTree1's call to the isPerfect() function yields the correct output.
+        self.assertEqual(self.binaryTree2.isPerfect(), False) # Check if binaryTree2's call to the isPerfect() function yields the correct output.
+        self.assertEqual(self.binaryTree3.isPerfect(), False) # Check if binaryTree3's call to the isPerfect() function yields the correct output.
+        self.assertEqual(self.binaryTree4.isPerfect(), False) # Check if binaryTree4's call to the isPerfect() function yields the correct output.
+        self.assertEqual(self.binaryTree8.isPerfect(), True) # Check if binaryTree4's call to the isPerfect() function yields the correct output.
 
 if __name__ == "__main__":
     unittest.main() # Run the unit tests to test the BinaryTree and BinarySearchTree classes.
@@ -693,7 +702,6 @@ if __name__ == "__main__":
     # vvv Sloppy Testing (To be removed) vvv
  
     # print(binaryTree1.contains(BinaryTree(2, BinaryTree(1)))) # print whether binaryTree1 contains a specific subtree
-    # print(binaryTree1.isPerfect()) # print a boolean denoting if the binary tree, binaryTree1 is perfect
 
     # print(binaryTree3.isDegenerate()) # print a boolean denoting if the binary tree, binaryTree3 is degenerate
     # print(binaryTree3.isBalanced()) # print a boolean denoting if the binary tree, binaryTree3 is balanced
