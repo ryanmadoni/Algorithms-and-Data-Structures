4bfee1c840e4a38a37f3e2ef8998eb297be5300e
ryanmadoni <radoni@stevens.edu>
2021-09-09 00:31:24 -0400

    added testing isFull

--- Binary Tree/BinaryTrees.py
+++ Binary Tree/BinaryTrees.py
@@ -682,6 +682,18 @@ class TestBinaryTreeMethods(unittest.TestCase):
         self.assertEqual(self.binaryTree3.isBalanced(), False) # Check if binaryTree3's call to the isBalanced() function yields the correct output.
         self.assertEqual(self.binaryTree4.isBalanced(), True) # Check if binaryTree4's call to the isBalanced() function yields the correct output.
 
+    def testIsFull(self):
+        """
+        A function to test the functionality of the class BinaryTree's isFull()'s function.
+        """
+        self.assertEqual(self.binaryTree1.isFull(), False) # Check if binaryTree1's call to the isFull() function yields the correct output.
+        self.assertEqual(self.binaryTree2.isFull(), False) # Check if binaryTree2's call to the isFull() function yields the correct output.
+        self.assertEqual(self.binaryTree3.isFull(), False) # Check if binaryTree3's call to the isFull() function yields the correct output.
+        self.assertEqual(self.binaryTree4.isFull(), True) # Check if binaryTree4's call to the isFull() function yields the correct output.
+        self.assertEqual(self.binaryTree8.isFull(), True) # Check if binaryTree8's call to the isFull() function yields the correct output.
+        self.assertEqual(self.binaryTree9.isFull(), False) # Check if binaryTree9's call to the isFull() function yields the correct output.
+
+
     def testIsComplete(self):
         """
         A function to test the functionality of the class BinaryTree's isComplete()'s function.
@@ -716,7 +728,6 @@ if __name__ == "__main__":
  
     # print(binaryTree1.contains(BinaryTree(2, BinaryTree(1)))) # print whether binaryTree1 contains a specific subtree
     # print(binaryTree3.isDegenerate()) # print a boolean denoting if the binary tree, binaryTree3 is degenerate
-    # print(binaryTree4.isFull()) # print a boolean denoting if the binary tree, binaryTree4 is full
     # print(binaryTree5.level(2)) # print level 2 of the binary tree, binaryTree5
     # print(binaryTree6.isBinarySearchTree()) # print a boolean denoting if the binary tree, binaryTree6 is a binary search tree
 
