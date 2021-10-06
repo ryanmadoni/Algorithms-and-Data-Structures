fa77626dc37ccdcab8c5ae347130debf1247d0ef
ryanmadoni <radoni@stevens.edu>
2021-10-06 00:04:56 -0400

    Added Sequences - Fibonacci

--- Sequences/Fibonacci.py
+++ Sequences/Fibonacci.py
@@ -7,19 +7,19 @@ Description:
 def recursiveFibonnaci(n):
     """
     An implementation of the Fibonacci sequence using recursion (a pretty poor implementation).  Takes in a
-    positive integer, n, and returns the nth Fibonacci number.
+    positive integer or zero, n, and returns the nth Fibonacci number.
     """
 
     # Make sure a valid n was entered.
-    if n < 1: 
+    if n < 0: 
         return None # Return None if a number less than 1 was entered.
 
     # The base case for the recursion
     if n <= 2:
-        return 1 # return 1 for the first and second Fibonacci number.
+        return 0 if n == 0 else 1 # return 1 for the first and second Fibonacci number.  Return 0 for the zeroth Fibonacci number.
 
     return recursiveFibonnaci(n - 1) + recursiveFibonnaci(n - 2) # Recurse to return the nth Fibonacci number.
 
 
 if __name__ == "__main__":
-    pass
+    print(recursiveFibonnaci(10))
