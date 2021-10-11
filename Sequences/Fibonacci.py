cbc60d2b23e140b11c2233994e93c7f323c05e44
ryanmadoni <radoni@stevens.edu>
2021-10-10 22:20:52 -0400

    Sloppy matrix fibonacci.  Add comments and fix soon

--- Sequences/Fibonacci.py
+++ Sequences/Fibonacci.py
@@ -7,8 +7,9 @@ Description: The Fibonacci Sequence
 import timeit
 import sys
 from math import sqrt
+import numpy as np
 
-sys.setrecursionlimit(10000) # Set the recursion limit to some high number for testing for the recrusive function.
+sys.setrecursionlimit(10000) # Set the recursion limit to some high number for testing for the recursive function.
 
 
 
@@ -48,6 +49,23 @@ def iterativeFibonacci(n):
         n0, n1 = temp, n0 + n1 # Set n0 to n1 and n1 to the sum of the previous two numbers.
 
     return n1 # return n0, the nth Fibonacci number.
+
+
+def matrixFibonacci(N):
+    """
+    """
+    A = np.array([[1, 1], [1, 0]])
+
+    def helper(n, F):
+        """
+        """
+
+        if n == N:
+            return F[0][0]
+        
+        return helper(n + 1, A @ F)
+
+    return helper(0, np.array([[0], [1]]))
 
 
 
@@ -94,11 +112,12 @@ if __name__ == "__main__":
 
     # print(recursiveFibonacci(25))
     # print(iterativeFibonacci(25))
-    # print(binetFibonacci(25))
+    print(binetFibonacci(25))
+    print(matrixFibonacci(25))
 
     # print(timeit.timeit(wrapper(recursiveFibonacci, 25), number = 10)) # Run and time the recursive definition.
-    print(timeit.timeit(wrapper(iterativeFibonacci, 1000), number = 100)) # Run and time the iterative definition.
-    print(timeit.timeit(wrapper(binetFibonacci, 1000), number = 100)) # Run and time the Binet definition.
+    # print(timeit.timeit(wrapper(iterativeFibonacci, 1000), number = 100)) # Run and time the iterative definition.
+    # print(timeit.timeit(wrapper(binetFibonacci, 1000), number = 100)) # Run and time the Binet definition.
 
     # The Binet calculation scales better with larger n as it is theta(1) while the iterative is still better
     # than the recrusive definition even though both are close to theta(n) since the overhead for the recursion
