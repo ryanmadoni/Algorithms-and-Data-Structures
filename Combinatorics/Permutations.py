c8983010dfc9a5c90a07a2b4e2da6d03c05462a0
ryanmadoni <radoni@stevens.edu>
2021-11-13 12:15:29 -0500

    Added Permutations.py

---
+++ Combinatorics/Permutations.py
@@ -0,0 +1,46 @@
+"""
+Author: Ryan Adoni
+Date: 7/29/2021
+Description: Functions relating to permutations. 
+"""
+
+
+from itertools import permutations
+
+def nextLexicographicPermutations(s):
+    """
+    Takes as input s, a string, and generates the next lexicographic permutation of s.  the next permutation is yielded and a 
+    generator can be used to continuously generate the subsequent permutations.
+    """
+    s = list(s) # Convert the string passed in into a list.
+    
+    # Loop until there are no more permutations.
+    while True:
+        i = j = len(s) - 1 # get the last index of the list representing the string and set i and j to it
+        
+        # Loop from the end of the list representing the string to the beginning to find the non-increasing suffix.
+        while i > 0 and s[i - 1] >= s[i]:
+            i -= 1 # Decrement the looping variable.
+        
+        # Check if i <= 0.  If this is true, there are no more lexicographic permutations.
+        if i <= 0:
+            return # Stop the generator.
+        
+        # Loop through from the end of the list representing the string to the beginning to find the successor to pivot.
+        while s[j] <= s[i - 1]:
+            j -= 1 # Decrement the looping variable.
+            
+        s[i - 1], s[j] = s[j], s[i - 1] # Swap the i-1th and jth elements in the list representing the string.
+        s[i : ] = s[len(s) - 1 : i - 1 : -1] # Reverse the suffix.
+        
+        yield "".join(s) # Convert the list back into a string and yield the result.
+
+
+
+def nthLexicographicPermutationPython(s, n):
+    """
+    Takes as input a string, s, and a number, n, and return the nth lexicographic permutation of s if it exists.
+    Returns None otherwise.  Uses Native Python.
+    """
+    perms = sorted(''.join(c) for c in permutations(s)) # Use the built in lexicographic permutation finder in Python.
+    return None if len(perms) < n - 1 else perms[n - 1] # Return the nth permutations if it exists.
