0aa349e90742671b79af230779c8dba72fb24d38
ryanmadoni <radoni@stevens.edu>
2021-11-15 00:14:59 -0500

    Small addition of Combinations

---
+++ Combinatorics/Combinations.py
@@ -0,0 +1,13 @@
+"""
+Author: Ryan Adoni
+Date: 9/22/2021
+Description: Functions relating to combinations. 
+"""
+
+from math import factorial
+
+def ncr(n, r):
+    """
+    Takes as input two positive integers, n and r, then calculates and returns (n r) = n! / r!(n - r)!
+    """
+    return factorial(n) / (factorial(r) * factorial(n - r)) # Calculate and return (n r).
