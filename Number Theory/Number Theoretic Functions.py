b6b3224071d4fb653af1d0faa7f8ecc7dd01301b
ryanmadoni <radoni@stevens.edu>
2021-11-27 01:52:02 -0500

    PEP8 Compliant

--- Number Theory/Number Theoretic Functions.py
+++ Number Theory/Number Theoretic Functions.py
@@ -9,100 +9,114 @@ from operator import mul
 from functools import reduce
 
 
+# A dictionary to memoize the prime factorizations
+# in the primeFactor function.
+factorizations = {}
 
-factorizations = {} # A dictionary to memoize the prime factorizations in the primeFactor function.
 
 def primeFactor(n):
     """
     Takes as input n, then calculates and returns the prime factorization of n.
-    """  
-    factors = [] # A list to store the prime factorization.
-    tempn = n # A variable to hold the original value of n.
-    
+    """
+    factors = []  # A list to store the prime factorization.
+    tempn = n  # A variable to hold the original value of n.
+
     # Removes all factors of 2 from the input number, n.
     while n % 2 == 0:
-        n >>= 1 # Right shift by 1 bit to quickly divide by 2.
-        factors.append(2) # Add 2 to the prime factorization.
-      
-    # If the input number, n, is now 1, n took the form 2^i, where i is in the set of natural numbers.
+        n >>= 1  # Right shift by 1 bit to quickly divide by 2.
+        factors.append(2)  # Add 2 to the prime factorization.
+
+    # If the input number, n, is now 1, n took the form
+    # 2^i, where i is in the set of natural numbers.
     if n == 1:
-        return factors # Therefore, return the prime factorization.
-    
-    factor = 3 # Start at 3, the second prime number as we have dealt with 2 already.
-    maxfactor = ceil(sqrt(n)) # Calculate the maximum factor as after this number there are repeats.
+        return factors  # Therefore, return the prime factorization.
+
+    factor = 3  # Start at 3, the second prime number.
+    maxfactor = ceil(sqrt(n))  # Calculate the maximum factor.
 
     # Loop until we reach the factor or n is not in factorizations.
     while factor <= maxfactor and n not in factorizations:
 
-        # Ff the input number, n, is divisible by the variable factor it means that we have found a new prime factor.
+        # If the input number, n, is divisible by the variable
+        # factor it means that we have found a new prime factor.
         if n % factor == 0:
-            n /= factor # Divide out the prime factor.
-            factors.append(factor) # Add the current factor to the prime factorization.
-            maxfactor = ceil(sqrt(n)) # Recalculate the maximum factor.
+            n /= factor  # Divide out the prime factor.
+            factors.append(factor)  # Append the current factor to factors.
+            maxfactor = ceil(sqrt(n))  # Recalculate the maximum factor.
         else:
-            factor += 2 # Add 2 as we start on 3, an odd number, and 2 is the only even prime number, so we can skip all even numbers.  This is why we deal with 2 first.
-    
+            # Add 2 as we start on 3, an odd number, and 2 is the only
+            # even prime number, so we can skip all even numbers.
+            # This is why 2 is dealt with first.
+            factor += 2
+
     # Check if n is in factorizations, i.e, this was calculated already.
     if n in factorizations:
-        factors += factorizations[n] # Append the dictionary value.
-        return factors # Return the factors.
-    
-    factors.append(round(n)) # Add the largest prime factor to the prime factorization list.
-    factorizations[tempn] = factors # Set the value of n in to the dictionary.
-    
-    return factors # Return the prime factorization of n.
+        factors += factorizations[n]  # Append the dictionary value.
+        return factors  # Return the factors.
 
+    factors.append(round(n))  # Append the largest prime factor to factors.
+    factorizations[tempn] = factors  # Set the value of n in to the dictionary.
+
+    return factors  # Return the prime factorization of n.
 
 
 def phi(n):
     """
-    Takes as input a positive integer, n, then returns phi(n), the number of numbers less than n which are 
-    relatively prime to n.  Phi is known as Euler's Totient Function.
+    Takes as input a positive integer, n, then returns phi(n),
+    the number of numbers less than n which are relatively
+    prime to n.  Phi is known as Euler's Totient Function.
     """
-    return round(n * reduce(mul, [1 - (1 / factor) for factor in set(primeFactor(n))])) # Calculate phi(n) using a formula using n's prime factorization.
 
+    # Calculate phi(n) using a formula using n's prime factorization.
+    factors = [1 - (1 / factor) for factor in set(primeFactor(n))]
+    return round(n * reduce(mul, factors))
 
 
 def little_omega(n):
     """
-    Takes as input a positive integer, n, then returns the number of distinct primes in the prime 
-    factorization of n.
+    Takes as input a positive integer, n, then returns the number
+    of distinct primes in the prime factorization of n.
     """
-    return len(set(primeFactor(n))) # Return the number of distinct primes in the prime factorization of n.
 
+    # Return the number of distinct primes
+    # in the prime factorization of n.
+    return len(set(primeFactor(n)))
 
 
 def big_omega(n):
     """
-    Takes as input a positive integer, n, then returns the number of non-distinct primes in the prime 
-    factorization of n.
+    Takes as input a positive integer, n, then returns the number
+    of non-distinct primes in the prime factorization of n.
     """
-    return len(primeFactor(n)) # Return the number of non-distinct primes in the prime factorization of n.
 
+    # Return the number of non-distinct primes
+    # in the prime factorization of n.
+    return len(primeFactor(n))
 
 
 def d(n):
     """
-    Takes as input n, then calculates and returns the sum of all proper divisors of n.
+    Takes as input n, then calculates and returns
+    the sum of all proper divisors of n.
     """
-    s = 1 # A counter to store the sum of all the divisors of n.
-    
+    s = 1  # A counter to store the sum of all the divisors of n.
+
     # Loop through all the numbers up to the square root of n.
     for d in range(2, floor(sqrt(n)) + 1):
-        
+
         # If d | n then d and n // d are divisors of n.
         if n % d == 0:
-            s += (d + n // d if d != n // d else d) # Add the divisors to the running sum.
-            
-    return s # Return the sum of the all the divisors of n.
+            s += (d + n // d if d != n // d else d)  # Add divisors to the sum.
 
+    return s  # Return the sum of the all the divisors of n.
 
 
 if __name__ == "__main__":
 
-    print(primeFactor(100)) # Calculate the prime factoriation of 100.
-    print(phi(100)) # Calculate the number of positive integers less than 100 that are coprime to 100.
-    print(little_omega(100)) # Calculate the number of distinct prime factors of 100.
-    print(big_omega(100)) # Calculate the number of non-distinct prime factors of 100.
+    print(primeFactor(100))  # Get the prime factoriation of 100.
+    print(phi(100))  # The number of numbers coprime to 100.
+    print(little_omega(100))  # The number of distinct prime factors of 100.
+    print(big_omega(100))  # The number of non-distinct prime factors of 100.
 
-    # Convert this file to a .ipynb and show formulas for number theoretic functions.
+    # Convert this file to a .ipynb and show formulas for number
+    # theoretic functions.
--- Number Theory/Sieves.py
+++ Number Theory/Sieves.py
@@ -6,51 +6,53 @@ Description: A file containing different sieves to find prime numbers.
 
 import timeit
 
+
 def sieveOfEratosthenes(n):
     """
-    Takes as input a positive integer, n, then calculates and returns all primes less than n using the 
-    Sieve of Eratosthenes.
+    Takes as input a positive integer, n, then calculates and returns
+    all primes less than n using the Sieve of Eratosthenes.
     """
-    prime = [1] * n # Create a list for the Sieve of Eratosthenes.
-    prime[0] = prime[1] = 0 # Set the first two numbers to not prime.
-    p = 2 # Start the counter at 2 as 1 would cross off all numbers.
-    
+    prime = [1] * n  # Create a list for the Sieve of Eratosthenes.
+    prime[0] = prime[1] = 0  # Set the first two numbers to not prime.
+    p = 2  # Start the counter at 2 as 1 would cross off all numbers.
+
     # Use the Sieve of Eratosthenes.
     while p * p <= n:
-        
+
         # Check if p is prime.
         if prime[p]:
-              
+
             # Remove all multiples of p from being prime.
             for i in range(p * 2, n, p):
-                prime[i] = 0 # Set the value to false since it is not prime.
+                prime[i] = 0  # Set the value to false since it is not prime.
 
-        p += 1 # Increment p by 1.
-         
-    return [p for p in range(n) if prime[p]] # Return a list of all primes less than n.
+        p += 1  # Increment p by 1.
 
+    return [p for p in range(n) if prime[p]]  # Return the primes less than n.
 
 
 def sieveOfSundaram(n):
     """
-    Takes as input a positive integer, n, then calculates and returns all primes less than n using the 
-    Sieve of Sundaram.
+    Takes as input a positive integer, n, then calculates and returns
+    all primes less than n using the Sieve of Sundaram.
     """
-    k = ((n - 2) >> 1) + 1 # Set the value k to the floor(n - 2) + 1.
-    prime = [1] * k # Set a list for the possible prime numbers.
+    k = ((n - 2) >> 1) + 1  # Set the value k to the floor(n - 2) + 1.
+    prime = [1] * k  # Set a list for the possible prime numbers.
 
     # Loop for all k values.
     for i in range(1, k):
-        j = i # Set i equal to j.
+        j = i  # Set i equal to j.
 
         # Loop until i+j + 2*i*j >= k.
         while (v := i + j + (i * j << 1)) < k:
-            prime[v] = 0 # Set the value in the prime list to 0.
-            j += 1 # Increment j.
+            prime[v] = 0  # Set the value in the prime list to 0.
+            j += 1  # Increment j.
 
-    return ([2] if n > 2 else []) + [2 * p + 1 for p in range(1, k) if prime[p]] # Return a list of all the primes less than n.
+    evenPrimes = ([2] if n > 2 else [])  # Even primes.
+    oddPrimes = [2 * p + 1 for p in range(1, k) if prime[p]]  # Odd primes.
 
-    
+    return evenPrimes + oddPrimes  # Return the primes less than n.
+
 
 def wrapper(func, *args, **kwargs):
     """
@@ -60,10 +62,9 @@ def wrapper(func, *args, **kwargs):
         """
         A helper function.
         """
-        return func(*args, **kwargs) # Call and return the wrapped function.
+        return func(*args, **kwargs)  # Call and return the wrapped function.
 
-    return wrapped # Return the wrapper.
-
+    return wrapped  # Return the wrapper.
 
 
 if __name__ == "__main__":
@@ -71,10 +72,12 @@ if __name__ == "__main__":
     # print(sieveOfEratosthenes(100)) # Find all primes less then 100.
     # print(sieveOfSundaram(100)) # Find all primes less then 100.
 
-    print(timeit.timeit(wrapper(sieveOfEratosthenes, 1000), number = 10)) # Run and time the recursive definition.
-    print(timeit.timeit(wrapper(sieveOfSundaram, 1000), number = 10)) # Run and time the matrix definition.
+    # Run and time the recursive and matrix definitions.
+    print(timeit.timeit(wrapper(sieveOfEratosthenes, 1000), number=10))
+    print(timeit.timeit(wrapper(sieveOfSundaram, 1000), number=10))
 
-    # My implementations of the Sieve of Eratosthenes runs faster 2x faster than my implemntation of
-    # the Sieve of Sundaram.
+    # My implementations of the Sieve of Eratosthenes runs faster
+    # 2x faster than my implementation of the Sieve of Sundaram.
 
-    
+    # Move Project Euler implementions Sieve of Atkin
+    # and Miller-Rabin prime test here and convert to ipynb.
